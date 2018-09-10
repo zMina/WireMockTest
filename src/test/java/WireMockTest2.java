3bab59351d18b63d92e64d1fd451cc69b68272c6
Zinaida Mazurkevych <zinaidamazurkevych@Zinaidas-MacBook-Pro.local>
2018-09-11 01:24:10 +0300

    practical task 4 b

---
+++ src/test/java/WireMockTest2.java
@@ -0,0 +1,38 @@
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static io.restassured.RestAssured.given;
+
+public class WireMockTest2 {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(4040);
+
+    @Before
+    public void setupStub() {
+
+        stubFor(post(urlEqualTo("/my/endpoint"))
+                .withRequestBody(matchingJsonPath("$[?(@.data == 'important')]"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "text/json")
+                        .withStatus(200)
+                        .withBodyFile("myfile.json")));
+    }
+
+
+    @Test
+    public void testStatusCode() {
+
+        given().contentType("text/json").body("{\"data\":\"important\"}").
+        when().
+            post("http://localhost:4040/my/endpoint").
+        then().
+            statusCode(200).
+        assertThat().body("name", org.hamcrest.Matchers.equalTo("zina"));
+    }
+
+}
