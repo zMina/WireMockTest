294bae7b6a573e0f641824544e13697abafe115b
Zinaida Mazurkevych <zinaida.mazurkevych@gmail.com>
2018-09-12 14:13:27 +0300

    add test plan

---
+++ src/test/java/TestPlan.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        HoverFlyTest.class,
+        WireMockTest2.class
+        })
+
+public class TestPlan
+{
+}
